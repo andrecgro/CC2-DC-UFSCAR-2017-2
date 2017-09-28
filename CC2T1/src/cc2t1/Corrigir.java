c5bbb702465ac5cd8d8eb49a445e7b005f2e4e03
André Rocha <andre.cgrocha@gmail.com>
2017-09-28 14:59:32 -0300

    Removendo flags antigas e inuteis ao projeto

--- CC2T1/src/cc2t1/Corrigir.java
+++ CC2T1/src/cc2t1/Corrigir.java
@@ -16,23 +16,9 @@ import org.antlr.v4.runtime.misc.ParseCancellationException;
 public class Corrigir {
 
     // Especifique o caminho dos casos de teste.
-    // Deve haver dois subdiretorios: entrada e saida
     private final static String CAMINHO_CASOS_TESTE = "/Users/andrecamargorocha/Documents/UFSCar/Compiladores II/CC2T1/casosDeTesteT1/1.arquivos_com_erros_sintaticos";
-    
-    // As flags GERA e VERIFICA são de uso do professor
-    // GERA = true significa que a saída vai ser gerada, sobrescrevendo qualquer
-    // conteudo do subdiretorio saida
-    // VERIFICA = true gera a saída junto com o conteúdo da entrada, para
-    // verificação
-    // Alunos: deixem ambas como "false"
-    private final static boolean GERA = false;
-    private final static boolean VERIFICA = false;
 
-    
-    // Descomente o método abaixo para testar
-    // Obs: este é o mesmo método que será usado pelo professor na correção
-    // A nota que você obtiver aqui será usada no cálculo de sua nota do trabalho
-    
+     
     public static void main(String[] args) throws IOException, RecognitionException {
         File diretorioCasosTeste = null;
         File[] casosTeste = null;
